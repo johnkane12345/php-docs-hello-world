b94f7b5c83fa2bb45353e65e4f0884106b395051
johnkane12345 <168829578+johnkane12345@users.noreply.github.com>
2024-05-03 17:06:11 +0200

    Update main.php

--- main.php
+++ main.php
@@ -11,15 +11,14 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 $dotenv->required(['CLIENT_ID', 'TENANT_ID', 'GRAPH_USER_SCOPES']);
 
-function initializeGraph(): void {
-    GraphHelper::initializeGraphForUserAuth();
-}
+initializeGraph();
 
 greetUser();
 
 $choice = -1;
 
-while ($choice != 0) {
+while ($choice != 0) 
+{
     echo('Please choose one of the following options:'.PHP_EOL);
     echo('0. Exit'.PHP_EOL);
     echo('1. Display access token'.PHP_EOL);
@@ -48,4 +47,57 @@ while ($choice != 0) {
     }
 }
 
+function initializeGraph(): void {
+    GraphHelper::initializeGraphForUserAuth();
+}
+
+function greetUser(): void {
+    try {
+        $user = GraphHelper::getUser();
+        print('Hello, '.$user->getDisplayName().'!'.PHP_EOL);
+
+        // For Work/school accounts, email is in Mail property
+        // Personal accounts, email is in UserPrincipalName
+        $email = $user->getMail();
+        if (empty($email)) {
+            $email = $user->getUserPrincipalName();
+        }
+        print('Email: '.$email.PHP_EOL.PHP_EOL);
+    } catch (Exception $e) {
+        print('Error getting user: '.$e->getMessage().PHP_EOL.PHP_EOL);
+    }
+}
+
+function displayAccessToken(): void {
+    // TODO
+}
+
+function listInbox(): void {
+    try {
+        $messages = GraphHelper::getInbox();
+
+        // Output each message's details
+        foreach ($messages->getValue() as $message) {
+            print('Message: '.$message->getSubject().PHP_EOL);
+            print('  From: '.$message->getFrom()->getEmailAddress()->getName().PHP_EOL);
+            $status = $message->getIsRead() ? "Read" : "Unread";
+            print('  Status: '.$status.PHP_EOL);
+            print('  Received: '.$message->getReceivedDateTime()->format(\DateTimeInterface::RFC2822).PHP_EOL);
+        }
+
+        $nextLink = $messages->getOdataNextLink();
+        $moreAvailable = isset($nextLink) && $nextLink != '' ? 'True' : 'False';
+        print(PHP_EOL.'More messages available? '.$moreAvailable.PHP_EOL.PHP_EOL);
+    } catch (Exception $e) {
+        print('Error getting user\'s inbox: '.$e->getMessage().PHP_EOL.PHP_EOL);
+    }
+}
+
+function sendMail(): void {
+    // TODO
+}
+
+function makeGraphCall(): void {
+    // TODO
+}
 ?>
